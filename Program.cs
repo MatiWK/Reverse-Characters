7986d6824d2e05dd129a081062cacd8b2135b290
Mateusz Weltrowski - Knopik <100945669+MatiWK@users.noreply.github.com>
2022-06-09 19:00:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmiana_Znaków
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj słowo: ");
+            string zdanie = Console.ReadLine();
+
+            char[] arrayZdanie = zdanie.ToCharArray();
+
+            Array.Reverse (arrayZdanie);
+            Console.Write("Odwrócone słowo: ");
+            Console.Write(arrayZdanie);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
